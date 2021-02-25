c4484bb82b0bd62c4d5e4abefc11e8ff7fce206b
Daniel Oliveira <oliveiradaniel@outlook.com>
2021-02-25 12:53:34 +0000

    minor fix

--- dlinklist.cpp
+++ dlinklist.cpp
@@ -6,10 +6,10 @@ int main()
 {
     std::cin.get();
     DoublyLinkedList<int> dl;
-    for (int i = 0; i< 20000; ++i)
+    for (int i = 0; i < 20000; ++i)
     {
         dl.push_back(i);
-        dl.push_front(i - 20);
+        dl.push_front(i - 20000);
     }
     std::cout << "Hello World!\n";
 }
